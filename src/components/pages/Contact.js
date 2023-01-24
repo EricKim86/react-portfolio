6fc4fba8212417b5843553acea58e56d1b678b98
EricKim86 <113047098+EricKim86@users.noreply.github.com>
2023-01-23 19:38:22 -0500

    updated Contact form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,12 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function Contact() {
+const styles = {
+  button: {
+    margin: '10px 0 0 0',
+  },
+};
+
+
+function Form() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    return name === 'Name' ? setName(value) : setEmail(value);
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    alert(`Message has been submitted.  Thank you ${name}!`);
+    setName('');
+    setEmail('');
+  };
+
   return (
-    <div>
-      <h1>Contact</h1>
-      <p>
-      Hey
-      </p>
+    <div className="container">
+      <form className="form-group">
+        <label for="inputName">Name</label>
+        <input
+          className="form-control"
+          value={name}
+          name="Name"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="Name"
+          id="inputName"
+        />
+        <label for="inputEmail">Email</label>
+        <input
+          className="form-control"
+          value={email}
+          name="Email"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="Email"
+          id="inputEmail"
+        />
+        <div class="form-group">
+          <label>Message</label>
+          <textarea class="form-control" rows="8"></textarea>
+        </div>
+        <button style={styles.button} className="btn btn-primary" type="button" onClick={handleFormSubmit}>
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
+
+export default Form;
